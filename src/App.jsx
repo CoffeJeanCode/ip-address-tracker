7cc037ea013b6316708087fc25d459075952d785
CoffeJeanCode <ortizmurciajeanpierre@gmail.com>
2022-07-17 04:53:30 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>IP Address Tracker</div>
+  )
+}
+
+// https://geo.ipify.org/api/v1?apiKey=at_jQTiZEFrzoxX3jrV9npv194ho5gWs&ipAddress=8.8.8.8
+
+export default App
